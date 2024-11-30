2381b25d148f17c0d4b99897d6b5b22b69a9c61c
navyap11 <ashwin.p.parmar@gmail.com>
2024-11-30 10:42:51 +0100

    f

---
+++ selling price and cost price.py
@@ -0,0 +1,8 @@
+costprice= int(input("Enter the cost: "))
+sellingprice= int(input("Enetr selling price: "))
+
+if sellingprice>costprice:
+    print ("Profit", sellingprice-costprice ,"rs")
+
+else :
+    print ("No Profit")
